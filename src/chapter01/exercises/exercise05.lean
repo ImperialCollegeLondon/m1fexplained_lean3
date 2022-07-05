5a3fff28b6f5fe63ba76d8bca7a5d19ceeb7dd23
xl <someemail@gmail.com>
2022-07-05 13:15:26 +0100

    solved part a

--- src/chapter01/exercises/exercise05.lean
+++ src/chapter01/exercises/exercise05.lean
@@ -1,11 +1,13 @@
 import tactic
+import data.nat.prime
+open nat
 
 -- True or false? n = 3 ↔ n^2-2n-3=0. If you think it's false
 -- then you'll have to modify the statement by putting it in brackets
 -- and adding a ¬ in front of it. 
-lemma part_a : ∀ n : ℤ, n = 3 ↔ n ^ 2 - 2 * n - 3 = 0 :=
+lemma part_a : ∀ n : ℤ, n = 3 → n ^ 2 - 2 * n - 3 = 0 :=
 begin
-  sorry
+  norm_num,
 end
 
 lemma part_b : ∀ n : ℤ, n ^ 2 - 2 * n - 3 = 0 → n = 3 := 
@@ -29,3 +31,4 @@ lemma part_e : ∀ a b : ℤ, (is_square a ∧ is_square b) → is_square (a * b
 begin
   sorry
 end
+
