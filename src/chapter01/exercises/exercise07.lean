287ede1841982c91dcd802c1ec93fe5dc7f8d3a2
xl <someemail@gmail.com>
2022-07-07 12:43:27 +0100

    solve a

--- src/chapter01/exercises/exercise07.lean
+++ src/chapter01/exercises/exercise07.lean
@@ -2,11 +2,13 @@ import tactic
 
 lemma part_a : ¬ (∀ n k : ℕ, n > 0 → k > 0 → k ∣ n ^ k - n) :=
 begin
-  sorry
+  intro h,
+  specialize h 2 4,
+  norm_num at h,
 end
 
 lemma part_b : ¬ (∀ n : ℕ, n > 0 → ∃ a b c : ℕ, n = a^2 + b^2 + c^2) :=
 begin
-  sorry
+
 end
 
