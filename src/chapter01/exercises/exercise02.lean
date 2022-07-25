fe7d0f4ede804f9955dfdc6ed2950d09aa036939
Sidharth Hariharan <fundamental.theor3m@gmail.com>
2022-07-25 11:58:19 +0400

    2b done too

--- src/chapter01/exercises/exercise02.lean
+++ src/chapter01/exercises/exercise02.lean
@@ -40,13 +40,44 @@ begin
   split,
   simp,
   rw [h0, E],
-  finish,
+  simp,
 end
 
 -- If X is either B,C,D,E and E ⊆ X ⊆ B, what else can we deduce about X? 
 -- Note that figuring out what to prove is part of the question
 lemma partb (X : set ℝ) (hX : X ∈ ({B,C,D,E} : set (set ℝ))) (h1 : E ⊆ X)
-(h2 : X ⊆ B) : sorry :=
+(h2 : X ⊆ B) : X ≠ D :=
 begin
-  sorry
+  intro hX',
+  rw hX' at *,
+  have hD : ¬ (E ⊆ D),
+  { have h0 : D = ({0} : set ℝ),
+    -- Any way I can get this hypothesis from the previous proof?
+    { simp_rw D,
+      ext,
+      split,
+      { intro hx,
+        simp at hx,
+        simp,
+        cases hx with hl hr,
+        cases hl with a ha,
+        rw ha at hr,
+        rw ha,
+        rw abs_lt at hr,
+        cases hr with hrl hrr,
+        by_contra hna,
+        norm_cast at *,
+        interval_cases a,
+        contradiction, },
+      intro hx,
+      simp at hx,
+      simp_rw hx,
+      simp,
+      use (0:ℤ),
+      simp, },
+    exfalso,
+    simp_rw [E, h0] at h1,
+    simp at h1,
+    exact h1, },
+  contradiction,
 end
