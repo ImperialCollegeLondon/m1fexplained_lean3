520580282cf73f7a15f028ad62a8f371a07a79d9
email@example.com <email@example.com>
2022-07-28 12:19:52 +0100

    solve 2

--- src/chapter01/exercises/exercise02.lean
+++ src/chapter01/exercises/exercise02.lean
@@ -6,10 +6,28 @@ def C : set ℝ := {x | 0 ≤ x ∧ x < 2}
 def D : set ℝ := {x | ∃ n : ℤ, x = n ∧ x^2 < 1}
 def E : set ℝ := {1}
 
+
+#print notation ¬ 
+#print notation ⊆ 
+#print notation *
+#print notation +
 -- Which pair of these sets has the property that neither is contained in the other?
 lemma parta : ∃ S T ∈ ({B, C, D, E} : set (set ℝ)), ¬ (S ⊆ T) ∧ ¬ (T ⊆ S) :=
 begin
-  sorry
+  use D,
+  use E,
+  split,
+  {simp,},
+  {split,{simp,},
+         {split,{
+          rw set.subset_def,
+          push_neg,use 0,split,{rw D,use 0,norm_num,},
+                               {rw E,simp,},
+         },{rw set.subset_def,
+            push_neg,use 1,split,{simp[E],},
+                                 {simp[D],},
+         },},
+                },
 end
 
 -- If X is either B,C,D,E and E ⊆ X ⊆ B, what else can we deduce about X? 
