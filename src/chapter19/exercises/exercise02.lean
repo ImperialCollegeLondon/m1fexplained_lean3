919a105118b7d48f5930425f6e60cfa2f7843515
xl <someemail@gmail.com>
2022-08-11 18:40:02 +0100

    solved

--- src/chapter19/exercises/exercise02.lean
+++ src/chapter19/exercises/exercise02.lean
@@ -15,16 +15,32 @@ def f (x : ℝ) := if 0 ≤ x ∧ x ≤ 1 then 2 * x else 1
 
 def g (x : ℝ) := if 0 ≤ x ∧ x ≤ 1 then x ^ 2 else 0
 
-example (x : ℝ) : (g ∘ f) x = if 0 ≤ x ∧ x ≤ 1/2 then 4 * x ^ 2 else if 0 ≤ x ∧ x ≤ 1/2 then 0 else 1 -- replace with formula for g ∘ f
+example (x : ℝ) : (g ∘ f) x = if 0 ≤ x ∧ x ≤ 1/2 then 4 * x ^ 2 else if 1/2 < x ∧ x ≤ 1 then 0 else 1 -- replace with formula for g ∘ f
 := 
 begin
-  split_ifs,
-  sorry,
+  change g (f x) = _,
+  unfold f,
+  unfold g,
+  split_ifs;
+  try {simp};
+  try {rw not_and_distrib at *};
+  try {cases h};
+  try {cases h_1};
+  try {cases h_2};
+  try {cases h_3};
+  try {linarith},
 end
 
 example (x : ℝ) : (f ∘ g) x = if 0 ≤ x ∧ x ≤ 1 then 2 * x ^ 2 else 0 
 := 
 begin
-  split_ifs,
-  sorry,
+  change f (g x) = _,
+  unfold f,
+  unfold g,
+  split_ifs;
+  try {simp};
+  try {rw not_and_distrib at *};
+  try {cases h};
+  try {cases h_1};
+  try {nlinarith},
 end
