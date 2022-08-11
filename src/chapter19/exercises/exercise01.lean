9f56e91915397b4fe57f29aec5577924c01cf66d
xl <someemail@gmail.com>
2022-08-11 17:44:21 +0100

    prime factorisation left

--- src/chapter19/exercises/exercise01.lean
+++ src/chapter19/exercises/exercise01.lean
@@ -1,6 +1,8 @@
 import tactic
 import data.real.sqrt
 import data.int.modeq
+import data.real.irrational
+import data.nat.factorization.prime_pow
 
 /-
 
@@ -127,7 +129,12 @@ lemma exercise03inj : injective f3 :=
 begin
   intros a b hab,
   unfold f3 at hab,
-  sorry
+  simp [mul_self_eq_mul_self_iff, pow_two] at hab,
+  rcases hab with h1 | h2,
+  {assumption},
+  {exfalso, suffices h : real.sqrt 2 = -(a + b) / 2, 
+  {norm_cast at h, apply irrational_sqrt_two, use (-(a + b) / 2), exact h.symm,},
+  {linear_combination h2 / 2}},
 end
 
 lemma exercise03surj : ¬ (surjective f3) :=
@@ -136,13 +143,15 @@ begin
   specialize h (-1),
   cases h with x h,
   unfold f3 at h,
-  sorry,
+  nlinarith,
 end
 
 lemma exercise04inj : injective f4 :=
 begin
-  intros x y h,
+  rintro ⟨a1, b1, c1⟩ ⟨a2, b2, c3⟩ h,
   unfold f4 at h,
+  simp,
+  -- should be solved by unique prime factorisation mathematically 
   sorry,
 end
 
@@ -151,15 +160,20 @@ begin
   intro h,
   specialize h 7,
   cases h with x h,
+  rcases x with ⟨a, b, c⟩,
   unfold f4 at h,
+  -- should be solved by considering prime factorisation 
   sorry,
 end
 
 lemma exercise05inj : ¬ (injective f5) :=
 begin
   intro h,
-  specialize h ⟨1,1,1⟩ ⟨2,2,0⟩,
-  sorry,
+  unfold injective at h,
+  have hp : f5 ⟨1,1,1⟩ = f5 ⟨2,2,0⟩,
+  {unfold f5 at *, norm_num},
+  specialize h hp, 
+  simpa using h,
 end
 
 lemma exercise05surj : ¬ (surjective f5) :=
@@ -167,7 +181,11 @@ begin
   intro h,
   specialize h 5,
   cases h with x h,
+  rcases x with ⟨a, b, c⟩,
   unfold f5 at h,
+  have hp : 2 ^ (a + c) * 3 ^ (b + c) = 5,
+  -- should be solved by considering prime factorsation
+  {sorry},
   sorry,
 end
 
@@ -175,12 +193,21 @@ lemma exercise06inj : injective f6 :=
 begin
   intros a b hab,
   unfold f6 at hab,
-  sorry,
+  revert hab,
+  apply quotient.induction_on₂ a b,
+  intros x y hab,
+  simp [s_def] at hab,
+  rw [quotient.eq, s_def],
+  convert int.modeq.sub_right 1 hab; simp,
 end
 
 lemma exercise06surj : surjective f6 :=
 begin
   intro y,
-  use (quotient (s-1)),
-  sorry,
+  apply quotient.induction_on y,
+  clear y,
+  intro a,
+  use ⟦a-1⟧,
+  unfold f6,
+  simp,
 end
