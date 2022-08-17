d8f86739c8c8d8926f873a4ec3ec205788ad6bcc
Natasha <natjo6626@protonmail.com>
2022-08-17 22:44:28 +0100

    chapter 2 exercise 3 a and b

--- src/chapter02/exercises/exercise03.lean
+++ src/chapter02/exercises/exercise03.lean
@@ -7,13 +7,31 @@ def rational (x : ℝ) :=
 /-- The product of two rational numbers is always rational. -/
 lemma part_a : ∀ {a b : ℝ}, rational a → rational b → rational (a * b) :=
 begin
-  sorry
+  intros a b,
+  rintro ⟨x1,y1,h1⟩ ⟨x2,y2,h2⟩,
+  use [x1*x2, y1*y2],
+  simp,
+  ring_nf,
+  rw [mul_inv, h1, h2],
+  ring_nf,
 end
 
 /-- The product of two irrational numbers is not always irrational. -/
 lemma part_b : ¬ ∀ {a b : ℝ}, irrational a → irrational b → irrational (a * b) :=
 begin
-  sorry
+  simp, 
+  use real.sqrt 2, 
+  split,
+  {exact irrational_sqrt_two,},
+  {use 1/(real.sqrt 2),
+  split,
+  {apply irrational.of_inv,
+  simp,
+  exact irrational_sqrt_two,},
+  {simp,
+  intro h,
+  apply irrational.ne_one,
+  {exact h,},{refl,},},},
 end
 
 /-- The product of two irrational numbers is not always rational. -/
