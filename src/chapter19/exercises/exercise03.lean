d11e50746a1475ac0488f2cf5d903ef4f4ce7858
xl <someemail@gmail.com>
2022-08-19 17:52:03 +0100

    solved 3

--- src/chapter19/exercises/exercise03.lean
+++ src/chapter19/exercises/exercise03.lean
@@ -9,31 +9,6 @@ Find the possibilities for f .
 
 def g : ℝ → ℝ := λ x, x^2+x+3
 
-example (x y : ℝ) : y^2 + y + 3 = x^2 - 3 * x + 5 → y = x - 2 ∨ y = -x + 1 :=
-begin
-  intro h,
-  have h' : y^2 + y + 3 = x^2 - 3 * x + 5 ↔ (y - (x - 2)) * (y - (-x + 1)) = 0,
-  {
-    split,
-    intro h,
-    nlinarith,
-    intro h, 
-    nlinarith,
-  },
-  simp [h', zero_eq_mul] at h,
-  sorry,
-end
-
-example (a b : ℝ) (h : a * b = 0) : (a = 0 ∨ b = 0) :=
-begin
-  exact zero_eq_mul.mp (eq.symm h),
-end
-
-example (a b : ℝ) (h : a - b = 0) : (a = b) :=
-begin
-  library_search,
-end
-
 -- Edit the "∀ x, f x = 37" part of the claim below and replace it with your answer 
 example (f : ℝ → ℝ) : ((g ∘ f) = λ x, x^2-3*x+5) ↔ ∀ x, f x = x - 2 ∨ f x = - x + 1 :=
 begin
@@ -46,7 +21,15 @@ begin
     have : y^2 + y + 3 = x^2 - 3 * x + 5 → y = x - 2 ∨ y = -x + 1,
     {
       intro h,
-      sorry
+      have h' : y^2 + y + 3 = x^2 - 3 * x + 5 ↔ (y - (x - 2)) * (y - (-x + 1)) = 0,
+      {
+        split,
+        intro h,
+        nlinarith,
+        intro h, 
+        nlinarith,
+      },
+      simpa [h', zero_eq_mul, sub_eq_zero] using h,
     },
     specialize this h,
     exact this,
