5860809f17aaeb0811b2b9bf9b9f96b25c29be46
Xialu3421 <108664927+Xialu3421@users.noreply.github.com>
2022-09-14 21:03:07 +0100

    Update src/chapter19/exercises/exercise01.lean
    Co-authored-by: Kevin Buzzard <k.buzzard@imperial.ac.uk>

--- src/chapter19/exercises/exercise01.lean
+++ src/chapter19/exercises/exercise01.lean
@@ -187,7 +187,7 @@ begin
   { intro h, 
     replace h := nat.prime.dvd_of_dvd_pow nat.prime_five h,
     norm_num at h, },
-    { intro h, 
+  { intro h, 
     replace h := nat.prime.dvd_of_dvd_pow nat.prime_five h,
     norm_num at h, },
   all_goals {exact pow_ne_zero _ (by norm_num)},
